2821c5726b5a093e5080bb48858762bd8638189b
Vivekanand Dubey <vd123754@gmail.com>
2026-03-31 08:35:50 +0530

    Payment logic initiated

--- qrticket/src/main/java/com/vivekanand/qrticket/controller/PaymentController.java
+++ qrticket/src/main/java/com/vivekanand/qrticket/controller/PaymentController.java
@@ -1,28 +1,28 @@
-package com.vivekanand.qrticket.controller;
-
-import com.razorpay.RazorpayException;
-import com.vivekanand.qrticket.service.PaymentService;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/api/payments")
-public class PaymentController {
-
-    @Autowired
-    private PaymentService paymentService;
-
-    @PostMapping("/create-order")
-    public String createOrder(@RequestParam int amount){
-
-        try {
-        	String b = paymentService.createOrder(amount, "INR", "recepient_100");
-        	System.out.println("\n\n "+b+"\n\n");
-            return b;
-        } catch (RazorpayException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    
-}
+//package com.vivekanand.qrticket.controller;
+//
+//import com.razorpay.RazorpayException;
+//import com.vivekanand.qrticket.service.PaymentService;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("/api/payments")
+//public class PaymentController {
+//
+//    @Autowired
+//    private PaymentService paymentService;
+//
+//    @PostMapping("/create-order")
+//    public String createOrder(@RequestParam int amount){
+//
+//        try {
+//        	String b = paymentService.createOrder(amount, "INR", "recepient_100");
+//        	System.out.println("\n\n "+b+"\n\n");
+//            return b;
+//        } catch (RazorpayException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//    
+//}
